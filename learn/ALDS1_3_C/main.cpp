215aa4f4dfc09b1f3f987959a5d40a897e3cc820
Tetta8 <tester.ta8@gmail.com>
2020-05-15 11:31:19 +0900

    連結リスト

---
+++ learn/ALDS1_3_C/main.cpp
@@ -0,0 +1,144 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int key;
+    Node *next, *prev;
+};
+
+Node *nil;
+
+Node* listSearch(int key){
+    Node *cur = nil->next;
+    while(cur != nil && cur->key != key){
+        cur = cur->next;
+    }
+
+    return cur;
+}
+
+void init(){
+    nil = (Node *)malloc(sizeof(Node));
+    nil->next = nil;
+    nil->prev = nil;
+}
+
+void printList(){
+    Node *cur = nil->next;
+    int isf = 0;
+    while(1){
+        if(cur == nil)break;
+        if(isf++ > 0) printf(" ");
+        printf("%d", cur->key);
+        cur = cur->next;
+    }
+    printf("\n");
+}
+
+void deleteNode(Node *t){
+    if(t == nil) return;
+    t->prev->next = t->next;
+    t->next->prev = t->prev;
+    free(t);
+}
+
+void deleteFirst(){
+    deleteNode(nil->next);
+}
+
+void deleteLast(){
+    deleteNode(nil->prev);
+}
+
+void deleteKey(int key){
+    deleteNode(listSearch(key));
+}
+
+void insert(int key){
+    Node *x = (Node *)malloc(sizeof(Node));
+    x->key = key;
+    x->next = nil->next;
+    nil->next->prev = x;
+    nil->next = x;
+    x->prev = nil;
+}
+
+
+int main(){
+  int key, n, i;
+  int size = 0;
+  char com[20];
+  int np = 0, nd = 0;
+  scanf("%d", &n);
+  init();
+  for ( i = 0; i < n; i++ ){
+    scanf("%s%d", com, &key);
+    cout << i << "count" << endl;
+    if ( com[0] == 'i' ) {
+        insert(key); np++; size++;
+    }
+    else if ( com[0] == 'd' ) {
+      if (strlen(com) > 6){
+	    if ( com[6] == 'F' ){
+            deleteFirst();
+        }else if ( com[6] == 'L' ) {
+            deleteLast();
+        }
+      } else {
+	deleteKey(key); nd++; 
+      }
+      size--;
+    }
+  }
+
+  printList();
+  return 0;
+}
+
+/*
+int main(){
+    int key, n, i;
+    int size = 0;
+    char com[20];
+    int np = 0, nd = 0;
+    scanf("%d", &n);
+    init();
+    for(i = 0; i < n; i++){
+        scanf("%s%d", com, &key);
+        // debug
+        cout << i << "inc" << endl;
+        if(com[0] == 'i'){
+            insert(key);
+            np++;
+            size++;
+            //debug
+            cout << "insert" << endl;
+        }else if(com[0] == 'd'){
+            if(strlen(com) > 6){
+                if(com[6] == 'F'){
+                    deleteFirst();
+                    //debug
+                    cout << "First" << endl;
+                }else if(com[6] == 'L'){
+                    deleteLast();
+                    //debug
+                    cout << "Last" << endl;
+                }else{}
+            }else{
+                deleteKey(key);
+                nd++;
+                //debug
+                cout << "nomal" << endl;
+            }    
+            size--;    
+        }
+    }
+    printList();
+
+    return 0;
+}
+*/
